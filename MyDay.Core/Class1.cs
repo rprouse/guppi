8676de1b949f37442bae56099b3cc3a236d13bd8
Rob Prouse <rob@prouse.org>
2020-04-09 10:08:58 -0400

    Projects to get started

---
+++ MyDay.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyDay.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ MyDay/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Alteridem.MyDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

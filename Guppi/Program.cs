725f671f599fdecf767898d610804d1bcd350c6d
Rob Prouse <rob@prouse.org>
2021-02-06 23:28:06 -0500

    Use top level statements for program entrypoint

--- Guppi/Program.cs
+++ Guppi/Program.cs
@@ -1,23 +1,16 @@
 using System;
-using System.Threading.Tasks;
+using Alteridem.Guppi;
 using Guppi.Application;
 using Guppi.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Alteridem.Guppi
-{
-    class Program
-    {
-        static async Task Main(string[] args) =>
-            await ConfigureServices()
-                    .GetRequiredService<IApplication>()
-                    .Run(args);
+await ConfigureServices()
+    .GetRequiredService<IApplication>()
+    .Run(args);
 
-        static private IServiceProvider ConfigureServices() =>
-            new ServiceCollection()
-                .AddTransient<IApplication, Application>()
-                .AddApplication()
-                .AddInfrastructure()
-                .BuildServiceProvider();
-    }
-}
+static IServiceProvider ConfigureServices() =>
+    new ServiceCollection()
+        .AddTransient<IApplication, Application>()
+        .AddApplication()
+        .AddInfrastructure()
+        .BuildServiceProvider();

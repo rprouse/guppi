d39554798bfa1588e683d8c6810f8bae19d4f894
Rob Prouse <rob@prouse.org>
2021-03-07 21:58:28 -0500

    Make sure we get today's data by getting the code for today

--- Guppi.Infrastructure/Services/Covid/CovidService.cs
+++ Guppi.Infrastructure/Services/Covid/CovidService.cs
@@ -6,10 +6,27 @@ namespace Guppi.Infrastructure.Services.Covid
 {
     public class CovidService : HttpService, ICovidService
     {
+        const string ROOT_URI = "https://static01.nyt.com/newsgraphics/2020/03/16/coronavirus-maps/";
+
         public async Task<CovidData> GetCovidData(Country country)
         {
-            var data = await GetData<NyTimesCovidData>($"https://static01.nyt.com/newsgraphics/2020/03/16/coronavirus-maps/f6f3e99e94435fad8987e9cc5e56436811b62995/data/timeseries/en/{country}.json");
+            var code = await GetCode();
+            var data = await GetData<NyTimesCovidData>($"{ROOT_URI}{code}/data/timeseries/en/{country}.json");
             return data.GetCovidData();
+        }
+
+        /// <summary>
+        /// Gets the code for today's covid data
+        /// </summary>
+        /// <returns></returns>
+        private async Task<string> GetCode()
+        {
+            // Load the Covid page and search it for a link to to the data we want
+            string html = await Client.GetStringAsync("https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html");
+            int start = html.IndexOf(ROOT_URI) + ROOT_URI.Length;
+            int end = html.IndexOf('/', start + 1);
+            string code = html.Substring(start, end - start);
+            return code;
         }
     }
 }

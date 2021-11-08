6cf452e519532116ea1d4dd59b5faab040176f47
Rob Prouse <rob@prouse.org>
2021-11-07 22:38:44 -0500

    Strip session= from AOC token

--- Guppi.Application/Configurations/AdventOfCodeConfiguration.cs
+++ Guppi.Application/Configurations/AdventOfCodeConfiguration.cs
@@ -4,8 +4,24 @@ namespace Guppi.Application.Configurations
 {
     class AdventOfCodeConfiguration : Configuration
     {
+        string _loginToken;
+
         [Display("Login Token")]
-        public string LoginToken { get; set; }
+        public string LoginToken 
+        {
+            get => _loginToken;
+            set
+            {
+                if(value?.StartsWith("session=", System.StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    _loginToken = value.Substring(8);
+                }
+                else
+                {
+                    _loginToken = value;
+                }
+            }
+        }
 
         [Display("AoC Solution Directory")]
         public string SolutionDirectory { get; set; } = @"C:\Src\Alteridem\AdventOfCode";
---
+++ Guppi.Tests/Application/Configuration/AdventOfCodeConfigurationTests.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+using Guppi.Application.Configurations;
+using NUnit.Framework;
+
+namespace Guppi.Tests.Application.Configuration
+{
+    [TestFixture]
+    public class AdventOfCodeConfigurationTests
+    {
+        [TestCase("", "")]
+        [TestCase("abcd", "abcd")]
+        [TestCase("session=abcd", "abcd")]
+        [TestCase("Session=abcd", "abcd")]
+        [TestCase("SESSION=abcd", "abcd")]
+        public void LoginTokenStripsSession(string value, string expected)
+        {
+            var config = new AdventOfCodeConfiguration();
+            config.LoginToken = value;
+            config.LoginToken.Should().Be(expected);
+        }
+    }
+}

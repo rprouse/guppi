568f66c8fdc1b65ea8dad8f18f55ff456af8abc1
Rob Prouse <rob@prouse.org>
2023-04-20 18:59:26 -0400

    Upload a hex file to an Agon Light computer. It may work with an RC2014 too?

--- Guppi.Console/Skills/SerialPortSkill.cs
+++ Guppi.Console/Skills/SerialPortSkill.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
+using System.IO;
+using System.Linq;
 using Guppi.Application.Extensions;
 using Guppi.Application.Services;
+using Spectre.Console;
 
 namespace Guppi.Console.Skills;
 
@@ -20,7 +24,13 @@ internal class SerialPortSkill : ISkill
         var ports = new Command("ports", "Lists all available serial ports");
         ports.Handler = CommandHandler.Create(() => ListPorts());
 
-        var hex = new Command("hex", "Converts a string to hex");
+        var hex = new Command("hex", "Uploads HEX files to the Agon Light computer over serial")
+        {
+            new Argument<FileInfo>("filename", "The HEX file to upload"),
+            new Argument<string>("port", () => "COM4", "The serial port to upload to"),
+            new Argument<int>("baud", () => 115200, "The baud rate to use")
+        };
+        hex.Handler = CommandHandler.Create<FileInfo, string, int>(UploadHex);
 
         return new[] { ports, hex };
     }
@@ -32,6 +42,38 @@ internal class SerialPortSkill : ISkill
         foreach (var port in ports)
         {
             System.Console.WriteLine(port);
+        }
+    }
+
+    private void UploadHex(FileInfo filename, string port, int baud)
+    {
+        AnsiConsoleHelper.TitleRule(":satellite_antenna: Upload HEX File to Agon Light");
+
+        AnsiConsole.MarkupLine($"[cyan]File:[/] {filename.Name}");
+        AnsiConsole.MarkupLine($"[cyan]Port:[/] {port}");
+        AnsiConsole.MarkupLine($"[cyan]Baud:[/] {baud}");
+        AnsiConsole.WriteLine();
+
+        if (!filename.Exists) 
+        { 
+            AnsiConsole.MarkupLine($"[yellow]:yellow_circle: File does not exist[/]");
+            return;
+        }
+
+        if (!_service.GetPorts().Contains(port))
+        {
+            AnsiConsole.MarkupLine($"[yellow]:yellow_circle: Port does not exist[/]");
+            return;
+        }
+
+        AnsiConsole.MarkupLine("[green]:green_circle: Uploading...[/]");
+        try
+        {
+            _service.UploadHex(port, baud, filename.FullName, (s) => AnsiConsole.MarkupLine(s));
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.MarkupLine($"[red]:red_circle: {ex.Message}[/]");
         }
     }
 }

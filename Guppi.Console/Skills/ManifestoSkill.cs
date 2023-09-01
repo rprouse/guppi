7992d97fe9f66abd049b5c0c7087dbc24a58ede5
Rob Prouse <rob@prouse.org>
2023-08-31 22:35:34 -0400

    Clean up the formatting

--- Guppi.Console/Skills/ManifestoSkill.cs
+++ Guppi.Console/Skills/ManifestoSkill.cs
@@ -23,7 +23,7 @@ internal class ManifestoSkill : ISkill
 
     private void ViewManifesto(string manifesto)
     {
-        var rows = System.Console.WindowHeight;
+        var rows = System.Console.WindowHeight - 5;
         var lines = manifesto.Split(Environment.NewLine);
         for (int i = 0; i < lines.Length; i++)
         {
@@ -55,56 +55,60 @@ internal class ManifestoSkill : ISkill
 
 
     const string HackerManifesto = @"
-[green]                       \/\The Conscience of a Hacker/\/[/]
+[green]               \/\The Conscience of a Hacker/\/[/]
 
-[green]                                      by[/]
+[green]                              by[/]
 
-[green]                               +++The Mentor+++[/]
+[green]                       +++The Mentor+++[/]
 
-[green]                          Written on January 8, 1986[/]
-[silver]=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=[/]
+[green]                  Written on January 8, 1986[/]
+[silver]=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=[/]
 
-[silver]        Another one got caught today, it's all over the papers.  ""Teenager[/]
+[silver]Another one got caught today, it's all over the papers.  ""Teenager[/]
 [silver]Arrested in Computer Crime Scandal"", ""Hacker Arrested after Bank Tampering""...[/]
-[green]        Damn kids.  They're all alike.[/]
+[green]Damn kids.  They're all alike.[/]
 
-[silver]        But did you, in your three-piece psychology and 1950's technobrain,[/]
+[silver]But did you, in your three-piece psychology and 1950's technobrain,[/]
 [silver]ever take a look behind the eyes of the hacker?  Did you ever wonder what[/]
 [silver]made him tick, what forces shaped him, what may have molded him?[/]
-[silver]        I am a hacker, enter my world...[/]
-[silver]        Mine is a world that begins with school... I'm smarter than most of[/]
-[silver]the other kids, this crap they teach us bores me...[/]
-[green]        Damn underachiever.  They're all alike.[/]
 
-[silver]        I'm in junior high or high school.  I've listened to teachers explain[/]
+[silver]I am a hacker, enter my world...[/]
+
+[silver]Mine is a world that begins with school... I'm smarter than most of[/]
+[silver]the other kids, this crap they teach us bores me...[/]
+[green]Damn underachiever.  They're all alike.[/]
+
+[silver]I'm in junior high or high school.  I've listened to teachers explain[/]
 [silver]for the fifteenth time how to reduce a fraction.  I understand it.  ""No, Ms.[/]
 [silver]Smith, I didn't show my work.  I did it in my head...""[/]
-[green]        Damn kid.  Probably copied it.  They're all alike.[/]
+[green]Damn kid.  Probably copied it.  They're all alike.[/]
 
-[silver]        I made a discovery today.  I found a computer.  Wait a second, this is[/]
+[silver]I made a discovery today.  I found a computer.  Wait a second, this is[/]
 [silver]cool.  It does what I want it to.  If it makes a mistake, it's because I[/]
 [silver]screwed it up.  Not because it doesn't like me...[/]
-[silver]                Or feels threatened by me...[/]
-[silver]                Or thinks I'm a smart ass...[/]
-[silver]                Or doesn't like teaching and shouldn't be here...[/]
-[green]        Damn kid.  All he does is play games.  They're all alike.[/]
+[silver]        Or feels threatened by me...[/]
+[silver]        Or thinks I'm a smart ass...[/]
+[silver]        Or doesn't like teaching and shouldn't be here...[/]
+[green]Damn kid.  All he does is play games.  They're all alike.[/]
 
-[silver]        And then it happened... a door opened to a world... rushing through[/]
+[silver]And then it happened... a door opened to a world... rushing through[/]
 [silver]the phone line like heroin through an addict's veins, an electronic pulse is[/]
 [silver]sent out, a refuge from the day-to-day incompetencies is sought... a board is[/]
 [silver]found.[/]
-[silver]        ""This is it... this is where I belong...""[/]
-[silver]        I know everyone here... even if I've never met them, never talked to[/]
-[silver]them, may never hear from them again... I know you all...[/]
-[green]        Damn kid.  Tying up the phone line again.  They're all alike...[/]
 
-[silver]        You bet your ass we're all alike... we've been spoon-fed baby food at[/]
+[silver]""This is it... this is where I belong...""[/]
+[silver]I know everyone here... even if I've never met them, never talked to[/]
+[silver]them, may never hear from them again... I know you all...[/]
+
+[green]Damn kid.  Tying up the phone line again.  They're all alike...[/]
+
+[silver]You bet your ass we're all alike... we've been spoon-fed baby food at[/]
 [silver]school when we hungered for steak... the bits of meat that you did let slip[/]
 [silver]through were pre-chewed and tasteless.  We've been dominated by sadists, or[/]
 [silver]ignored by the apathetic.  The few that had something to teach found us will-[/]
 [silver]ing pupils, but those few are like drops of water in the desert.[/]
 
-[silver]        This is our world now... the world of the electron and the switch, the[/]
+[silver]This is our world now... the world of the electron and the switch, the[/]
 [silver]beauty of the baud.  We make use of a service already existing without paying[/]
 [silver]for what could be dirt-cheap if it wasn't run by profiteering gluttons, and[/]
 [silver]you call us criminals.  We explore... and you call us criminals.  We seek[/]
@@ -113,13 +117,13 @@ internal class ManifestoSkill : ISkill
 [silver]You build atomic bombs, you wage wars, you murder, cheat, and lie to us[/]
 [silver]and try to make us believe it's for our own good, yet we're the criminals.[/]
 
-[silver]        Yes, I am a criminal.  My crime is that of curiosity.  My crime is[/]
+[silver]Yes, I am a criminal.  My crime is that of curiosity.  My crime is[/]
 [silver]that of judging people by what they say and think, not what they look like.[/]
 [silver]My crime is that of outsmarting you, something that you will never forgive me[/]
 [silver]for.[/]
 
-[green]        I am a hacker, and this is my manifesto.  You may stop this individual,[/]
+[green]I am a hacker, and this is my manifesto.  You may stop this individual,[/]
 [green]but you can't stop us all... after all, we're all alike.[/]
 
-[silver]                               +++The Mentor+++[/]";
+[silver]                       +++The Mentor+++[/]";
 }
